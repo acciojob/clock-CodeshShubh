8c0084766db06573344246f0dc22f3548b0424fc
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-15 23:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const p = document.getElementById('timer');
+
+
+
+setInterval(()=>{
+	const Date = new Date();
+	 p.innerHTML = Date.toLocaleString();
+},1000)
+
